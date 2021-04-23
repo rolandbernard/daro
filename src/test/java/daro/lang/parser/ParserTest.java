fcd5e67cf7c66e88b9067623043904b408bc35fe
Roland Bernard <rolbernard@unibz.it>
2021-04-23 23:11:00 +0200

    Added some parser tests

--- src/test/java/daro/lang/parser/ParserTest.java
+++ src/test/java/daro/lang/parser/ParserTest.java
@@ -33,57 +33,127 @@ public class ParserTest {
     @Test
     void complexExpression() {
         AstNode ast = Parser.parseSourceCode(
-            "1 == 2 << 1 + 4 * 5 ^ 3 % 9 / 7 || !(~3 >= -4) && +4 < '\\'' || foo(1, 2, 3, bar[12 + 4])"
+            "x = 1 == 2 << 1 + 4 * 5 ^ 3 % 9 / 7 || !(~3 >= -4) && +4 < '\\'' || foo(1, 2, 3, bar[12 + 4])"
         );
         assertEquals(new AstBlock(null, new AstNode[] {
-            new AstOr(null,
+            new AstAssignment(null,
+                new AstSymbol(null, "x"),
                 new AstOr(null,
-                    new AstEqual(null,
-                        new AstInteger(null, 1),
-                        new AstBitwiseXor(null,
-                            new AstShiftLeft(null,
-                                new AstInteger(null, 2),
-                                new AstAddition(null,
-                                    new AstInteger(null, 1),
-                                    new AstMultiply(null,
-                                        new AstInteger(null, 4),
-                                        new AstInteger(null, 5)
+                    new AstOr(null,
+                        new AstEqual(null,
+                            new AstInteger(null, 1),
+                            new AstBitwiseXor(null,
+                                new AstShiftLeft(null,
+                                    new AstInteger(null, 2),
+                                    new AstAddition(null,
+                                        new AstInteger(null, 1),
+                                        new AstMultiply(null,
+                                            new AstInteger(null, 4),
+                                            new AstInteger(null, 5)
+                                        )
                                     )
+                                ),
+                                new AstDivide(null,
+                                    new AstRemainder(null,
+                                        new AstInteger(null, 3),
+                                        new AstInteger(null, 9)
+                                    ),
+                                    new AstInteger(null, 7)
+                                )
+                            )
+                        ),
+                        new AstAnd(null,
+                            new AstNot(null,
+                                new AstMoreOrEqual(null, 
+                                    new AstBitwiseNot(null, new AstInteger(null, 3)),
+                                    new AstNegative(null, new AstInteger(null, 4))
                                 )
                             ),
-                            new AstDivide(null,
-                                new AstRemainder(null,
-                                    new AstInteger(null, 3),
-                                    new AstInteger(null, 9)
-                                ),
-                                new AstInteger(null, 7)
+                            new AstLessThan(null,
+                                new AstPositive(null, new AstInteger(null, 4)),
+                                new AstCharacter(null, '\'')
                             )
                         )
                     ),
-                    new AstAnd(null,
-                        new AstNot(null,
-                            new AstMoreOrEqual(null, 
-                                new AstBitwiseNot(null, new AstInteger(null, 3)),
-                                new AstNegative(null, new AstInteger(null, 4))
+                    new AstCall(null,
+                        new AstSymbol(null, "foo"),
+                        new AstNode[] {
+                            new AstInteger(null, 1),
+                            new AstInteger(null, 2),
+                            new AstInteger(null, 3),
+                            new AstIndex(null,
+                                new AstSymbol(null, "bar"),
+                                new AstAddition(null, new AstInteger(null, 12), new AstInteger(null, 4))
                             )
-                        ),
-                        new AstLessThan(null,
-                            new AstPositive(null, new AstInteger(null, 4)),
-                            new AstCharacter(null, '\'')
-                        )
+                        }
+                    )
+                )
+            )
+        }), ast);
+    }
+
+    @Test
+    void newExpression() {
+        AstNode ast = Parser.parseSourceCode("new []test + new test");
+        assertEquals(new AstBlock(null, new AstNode[] {
+            new AstAddition(null, 
+                new AstNew(null,
+                    new AstArray(null, new AstSymbol(null, "test")),
+                    null
+                ),
+                new AstNew(null,
+                    new AstSymbol(null, "test"),
+                    null
+                )
+            )
+        }), ast);
+    }
+
+    @Test
+    void newInitializedExpression() {
+        AstNode ast = Parser.parseSourceCode("new []test{1, 2, {3, 4, { a = 5, b = 6 }}}");
+        assertEquals(new AstBlock(null, new AstNode[] {
+            new AstNew(null,
+                new AstArray(null, new AstSymbol(null, "test")),
+                new AstInitializer(null, new AstNode[] {
+                    new AstInteger(null, 1), new AstInteger(null, 2),
+                    new AstInitializer(null, new AstNode[] {
+                        new AstInteger(null, 3), new AstInteger(null, 4),
+                        new AstInitializer(null, new AstNode[] {
+                            new AstAssignment(null,
+                                new AstSymbol(null, "a"),
+                                new AstInteger(null, 5)
+                            ),
+                            new AstAssignment(null,
+                                new AstSymbol(null, "b"),
+                                new AstInteger(null, 6)
+                            )
+                        })
+                    })
+                })
+            )
+        }), ast);
+    }
+
+    @Test
+    void simpleIfElseStatement() {
+        AstNode ast = Parser.parseSourceCode("if (1 == 4 + 5) foo() else bar()");
+        assertEquals(new AstBlock(null, new AstNode[] {
+            new AstIfElse(null,
+                new AstEqual(null,
+                    new AstInteger(null, 1),
+                    new AstAddition(null,
+                        new AstInteger(null, 4),
+                        new AstInteger(null, 5)
                     )
                 ),
                 new AstCall(null,
                     new AstSymbol(null, "foo"),
-                    new AstNode[] {
-                        new AstInteger(null, 1),
-                        new AstInteger(null, 2),
-                        new AstInteger(null, 3),
-                        new AstIndex(null,
-                            new AstSymbol(null, "bar"),
-                            new AstAddition(null, new AstInteger(null, 12), new AstInteger(null, 4))
-                        )
-                    }
+                    new AstNode[0]
+                ),
+                new AstCall(null,
+                    new AstSymbol(null, "bar"),
+                    new AstNode[0]
                 )
             )
         }), ast);
