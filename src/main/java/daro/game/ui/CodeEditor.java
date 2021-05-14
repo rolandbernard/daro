095c120931868e0844382e5017fef904f1eab550
Planoetscher Daniel (Student Com20) <daniel.planoetscher@stud-inf.unibz.it>
2021-05-14 10:06:53 +0200

    CodeEditor now using RichTextFX dependency

--- src/main/java/daro/game/ui/CodeEditor.java
+++ src/main/java/daro/game/ui/CodeEditor.java
@@ -1,31 +1,36 @@
 package daro.game.ui;
 
 import daro.game.main.Game;
-import javafx.scene.control.TextArea;
+import javafx.beans.value.ObservableValue;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
 
 import java.util.HashMap;
 
 
-public class CodeEditor extends TextArea {
+public class CodeEditor extends CodeArea {
     private int lastTypePosition = -1;
     private HashMap<Character, Character> followingCharacter = new HashMap<>();
 
     public CodeEditor(String defaultText) {
-        this.setText(defaultText);
+        super(defaultText);
         this.setHeight(Game.HEIGHT);
-        this.setWidth(Game.WIDTH);
+        this.setPrefWidth(Game.WIDTH);
         initFollowingCharacter();
-        this.setStyle("-fx-fill: #1A0A47; -fx-font-size: 20px; -fx-font-family: Consolas, monospace;");
-        this.textProperty().addListener((observable, oldValue, newValue) -> {
-            int position = this.getCaretPosition();
-            if (oldValue.length() < newValue.length() && lastTypePosition != position) {
-                char lastCharacter = newValue.charAt(position);
-                lastTypePosition = position;
-                if(followingCharacter.containsKey(lastCharacter)) {
-                    this.insertText(position + 1, followingCharacter.get(lastCharacter).toString());
-                }
-            }
-        });
+        this.setParagraphGraphicFactory(LineNumberFactory.get(this));
+        this.setStyle("-fx-background-color: #1A0A47; -fx-font-size: 20px; -fx-font-family: Consolas, monospace;");
+        this.textProperty().addListener(this::handleTextChange);
+    }
+
+    private void handleTextChange(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+        int position = this.getCaretPosition() - 1;
+        if (position >= 0 && oldValue.length() < newValue.length() && lastTypePosition != position) {
+            char lastCharacter = newValue.charAt(position);
+            System.out.println(lastCharacter);
+            lastTypePosition = position;
+            if (followingCharacter.containsKey(lastCharacter))
+                this.insertText(position + 1, followingCharacter.get(lastCharacter).toString());
+        }
     }
 
     private void initFollowingCharacter() {
--- src/test/java/daro/game/GameTest.java
+++
@@ -1,9 +0,0 @@
-package daro.game;
-
-import org.junit.jupiter.api.Test;
-import org.testfx.api.FxAssert;
-import org.testfx.matcher.control.LabeledMatchers;
-
-public class GameTest {
-
-}
