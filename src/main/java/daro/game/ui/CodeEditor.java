6831e1b1524c3f0d1f1ea697b272c47ab82a2183
Planoetscher Daniel (Student Com20) <daniel.planoetscher@stud-inf.unibz.it>
2021-05-14 12:29:54 +0200

    basic styling of codeeditor

--- src/main/java/daro/game/ui/CodeEditor.java
+++ src/main/java/daro/game/ui/CodeEditor.java
@@ -15,49 +15,49 @@ import java.util.stream.Collectors;
 
 public class CodeEditor extends CodeArea {
     private static final String[] keywords = {"fn", "break"};
-    private static final Pattern SYNTAX_HIGHLIGHT = Pattern.compile("(?<KEYWORD>(" + Arrays.stream(keywords).map(keyword -> "\\b" + keyword + "\\b").collect(Collectors.joining("|"))+ "))");
+    private static final Pattern SYNTAX_HIGHLIGHT = Pattern.compile("(?<KEYWORD>(" + Arrays.stream(keywords).map(keyword -> "(\\b" + keyword + "\\b)").collect(Collectors.joining("|")) + "))");
+    private static final HashSet<Character> lineBreakExtra = new HashSet<>(Set.of('{'));
+    private static final HashMap<Character, Character> followingCharacter = new HashMap<>();
 
     private int lastTypePosition = -1;
-    private HashSet<Character> lineBreakExtra = new HashSet<>(Set.of('{'));
-    private HashMap<Character, Character> followingCharacter = new HashMap<>();
 
     public CodeEditor(String defaultText) {
         super(defaultText);
+        this.getStyleClass().add("code-editor");
         this.setHeight(Game.HEIGHT);
         this.setPrefWidth(Game.WIDTH);
         initFollowingCharacter();
         this.setParagraphGraphicFactory(LineNumberFactory.get(this));
-        this.setStyle("-fx-background-color: #1A0A47; -fx-font-size: 16px; -fx-font-family: Consolas, monospace;" +
-                "-fx-text-fill: #fff");
+        this.setStyle("-fx-background-color: #200D57;");
         this.textProperty().addListener(this::handleTextChange);
     }
 
     private void handleTextChange(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
         this.setStyleSpans(0, computeHighlighting(newValue));
         int position = this.getCaretPosition() - 1;
-        if (position >= 0 && oldValue.length() < newValue.length() && lastTypePosition != position) {
+        if (position > 0 && oldValue.length() < newValue.length() && lastTypePosition != position) {
             char lastCharacter = newValue.charAt(position);
             lastTypePosition = position;
-            if (followingCharacter.containsKey(lastCharacter))
+            if (followingCharacter.containsKey(lastCharacter)) {
                 this.insertText(position + 1, followingCharacter.get(lastCharacter).toString());
-            /*else {
+            } else {
                 char preLastCharacter = newValue.charAt(position - 1);
-                if(lastCharacter == '\r' && (position - 1) > 0 && lineBreakExtra.contains(preLastCharacter)) {
-                    this.insertText(position, "\t\n");
-                }
-            }*/
-
+                //if (lastCharacter == '\n' && (position - 1) > 0 && lineBreakExtra.contains(preLastCharacter)) {
+                //    this.insertText(position, "\t\n");
+                //}
+            }
         }
     }
+
     private static StyleSpans<Collection<String>> computeHighlighting(String text) {
 
         Matcher matcher = SYNTAX_HIGHLIGHT.matcher(text);
         int lastKwEnd = 0;
         StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
-        while(matcher.find()) {
+        while (matcher.find()) {
 
             spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
-            if(matcher.group("KEYWORD") != null) {
+            if (matcher.group("KEYWORD") != null) {
                 spansBuilder.add(Collections.singleton("keyword"), matcher.end() - matcher.start());
             }
             lastKwEnd = matcher.end();
