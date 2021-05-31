713bd8ca4d1d3f1515790488b0b53a1b2f753a69
Planoetscher Daniel (Student Com20) <daniel.planoetscher@stud-inf.unibz.it>
2021-05-31 15:42:48 +0200

    using streams

--- src/main/java/daro/game/validation/Validation.java
+++ src/main/java/daro/game/validation/Validation.java
@@ -4,8 +4,8 @@ import daro.lang.interpreter.Interpreter;
 import daro.lang.interpreter.InterpreterException;
 import daro.lang.values.UserObject;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Validation {
     private long id;
@@ -14,6 +14,12 @@ public class Validation {
     private UserObject expected;
 
 
+    /**
+     * Generates a test for daro without an expected value.
+     * @param id number of test for the level
+     * @param type a type of test
+     * @param source variable / function call that has to be tested
+     */
     public Validation(long id, ValidationType type, String source) {
         if (type.needsExpectedValue())
             throw new IllegalArgumentException("ValidationType " + type + " needs an expected value.");
@@ -22,6 +28,13 @@ public class Validation {
         this.source = source;
     }
 
+    /**
+     * Generates a test for daro with an expected value.
+     * @param id number of test for the level
+     * @param type a type of test
+     * @param source variable / function call that has to be tested
+     * @param expected the expected value. Either a simple value or an array in the form of e.g. [2, 3, 4]
+     */
     public Validation(long id, ValidationType type, String source, String expected) {
         if (!type.needsExpectedValue())
             throw new IllegalArgumentException("ValidationType " + type + " doesn't need an expected value.");
@@ -32,14 +45,25 @@ public class Validation {
     }
 
 
+    /**
+     * Runs all the tests of a list on a specific code
+     *
+     * @param code the code that has to be validated
+     * @param validations a list containing tests that have to be run
+     * @return a list of test results
+     */
     public static List<ValidationResult> run(String code, List<Validation> validations) {
-        List<ValidationResult> result = new ArrayList<>();
-        for (Validation validation : validations) {
-            result.add(validation.runTest(code));
-        }
-        return result;
+        return validations.stream()
+                .map(v -> v.runTest(code))
+                .collect(Collectors.toList());
     }
 
+    /**
+     * Runs a test on a code.
+     *
+     * @param code the Code that has to be tested
+     * @return a ValidationResult Object, containing information about the test
+     */
     private ValidationResult runTest(String code) {
         Interpreter interpreter = new Interpreter();
         boolean success = false;
@@ -66,6 +90,13 @@ public class Validation {
         return new ValidationResult(id, success, expected.toString(), givenResult);
     }
 
+    /**
+     * Parses the expected result from a user input and returns the UserObject.
+     * User can input an array with e.g. [10, 20, 30]
+     *
+     * @param expected expected string value
+     * @return UserObject to compare to actual value
+     */
     private UserObject parseExpectedResult(String expected) {
         UserObject expectedResult;
         Interpreter interpreter = new Interpreter();
